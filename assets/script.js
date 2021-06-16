28f3d31ea49f1857af82029d4ace53c01ae01f04
MojosBeans100 <lucy.colley@btinternet.com>
2021-06-16 20:47:42 +0000

    Add first functions to create difference screens the user sees during the game

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+
+/**
+ * This function pulls up the main introduction screen, welcoming the user to the game,
+ * a brief summary as to what the game is
+ * and the user can input their name
+ */
+function introScreen() {
+
+}
+
+/**
+ * This screen gives the user some game options
+ * Easy, Medium, Hard - the difference between these should be obvious so add this detail
+ * Game length ie number of questions
+ */
+function gameOptionsScreen() {
+
+}
+
+/**
+ * This screen gives the user instructions as to how the game will work
+ */
+function instructionsScreen() {
+
+}
+
+/**
+ * This screen shows the actual questions and multiple choice answers,
+ * score tallies, countdown clock
+ */
+function playGameScreen() {
+
+}
+
+
+/**
+ * This screen shows the end score, congrats message,
+ * perhaps input email address to send answers to
+ */
+function endGameScreen() {
+
+}
